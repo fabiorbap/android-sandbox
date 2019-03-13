d30d6d43159b87cc1845898cbfd5b8d8a0e18d09
Fabio Rachid <fabiorbap@gmail.com>
2019-03-12 22:47:43 -0300

    Implemented screens and navigation

--- app/src/main/java/br/com/frachid/sandbox/viewcontrollers/UserFormActivity.kt
+++ app/src/main/java/br/com/frachid/sandbox/viewcontrollers/UserFormActivity.kt
@@ -1,31 +1,22 @@
 package br.com.frachid.sandbox.viewcontrollers
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import br.com.frachid.sandbox.R
-import kotlinx.android.synthetic.main.activity_user_list.*
 
-class UserListActivity : AppCompatActivity() {
+class UserFormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_user_list)
-        setupFAB()
+        setContentView(R.layout.activity_user_form)
         setupToolbar()
         super.onCreate(savedInstanceState)
-    }
-
-    private fun setupFAB() {
-        userListAddUser.setOnClickListener {
-            startActivity(Intent(this, UserFormActivity::class.java))
-        }
     }
 
     private fun setupToolbar(){
         supportActionBar?.let {
             with(it) {
-                title = getString(R.string.user_list_title)
+                title = getString(R.string.user_forms_title)
                 setDisplayHomeAsUpEnabled(true)
             }
         }
